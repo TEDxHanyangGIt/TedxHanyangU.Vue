dcd5e4a9ad551d457d866b644b060e9bee01b5e1
94wogus <94wogus@gmail.com>
2020-05-08 23:44:06 +0900

    build in develop and Ready to deploy on master branch

--- deploy.sh
+++ deploy.sh
@@ -4,5 +4,5 @@ cp ./404.html ./docs/404.html
 cp ./CNAME ./docs/CNAME
 
 git add -A
-git commit -am 'build and deploy'
+git commit -am 'build in develop and Ready to deploy on master branch'
 git push
