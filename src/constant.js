08d58191819d31a7888ada8ff825ad823014bad9
94wogus <94wogus@gmail.com>
2020-05-09 11:14:11 +0900

    Fix: del Not used views

--- src/constant.js
+++ src/constant.js
@@ -5,16 +5,6 @@ export const menuList = [
         icon: "fas fa-home grey-color"
     },
     {
-        name: "Post",
-        route_name: "post",
-        icon: "fas fa-home grey-color"
-    },
-    {
-        name: "Project",
-        route_name: "project",
-        icon: "fas fa-home grey-color"
-    },
-    {
         name: "About",
         route_name: "about",
         icon: "fas fa-home grey-color"
