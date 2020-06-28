e9dc9e8bb8438c020078613d3591791f78166b0d
94wogus <94wogus@gmail.com>
2020-06-28 13:46:03 +0900

    Add: Youtube Iframe View

---
+++ src/components/common/YouTubeIframe.vue
@@ -0,0 +1,117 @@
+<template>
+    <div class="VideoFrame">
+        <div class="VideoBox">
+            <h1 style="color: white; margin-bottom: 5vh; margin-top: 5vh">Tedx Rules</h1>
+            <div class="Video">
+                <MyPlayer
+                    ref="p"
+                    :ytid="ytid"
+                    :yturl="yturl"
+                    :width="videoWindow.width"
+                    :height="videoWindow.height"
+                    :player-vars="{controls: 0}"
+                    @read="onPlayerReady"
+                />
+            </div>
+        </div>
+        <div class="mx-10 goAbout">
+            <div id="pendulum">
+                <v-btn x-large rounded color="white" style="font-weight: bold;" >
+                    Go About<v-icon right>fa-arrow-right</v-icon>
+                </v-btn>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import { Player } from 'vue-youtube-iframe-api'
+
+export default {
+    name: "YouTubeVideo",
+    props : ["ytid", "yturl", "videoplay"],
+    data(){
+        return{
+            videoWindow:{
+                width:0,
+                height:0
+            }
+        }
+    },
+    created(){
+        window.addEventListener('resize', this.handleResize);
+        this.handleResize();
+    },
+    methods: {
+        goAbout(){
+            this.$router.push({name: "about"});
+        },
+        onPlayerReady () {
+            console.log(this.$refs.p.player)
+        },
+        play() {
+            this.$refs.p.player.playVideo()
+        },
+        pause() {
+            this.$refs.p.player.pauseVideo()
+        },
+        handleResize() {
+            this.videoWindow.width = Math.max(window.innerWidth * 0.6, 300);
+            this.videoWindow.height = Math.max(window.innerWidth *  0.3375, 168.75);
+        }
+    },
+    components: {
+        MyPlayer: Player
+    },
+    watch: {
+        videoplay: function () {
+            if(this.videoplay){
+                this.play()
+            } else{
+                this.pause()
+            }
+        }
+    },
+    destroyed() {
+        window.removeEventListener('resize', this.handleResize);
+    },
+}
+</script>
+
+<style scoped>
+.VideoFrame{
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: flex-end;
+    height: calc(100vh - 64px);
+}
+.Video{
+  padding: 10px;
+  border: 1px solid white; 
+}
+.VideoBox{
+    position: absolute;
+    align-self: flex-start;
+}
+.goAbout{
+    color: white;
+    margin-bottom: 2vh;
+}
+
+@media(max-width:600px) {
+    .goAbout{
+        margin-bottom: 10vh;
+    }
+}
+
+@keyframes pendulum  {
+    from { margin-left: 40px;}
+    to { margin-left:10px; }
+}
+#pendulum {
+    animation: pendulum 0.5s infinite;
+    animation-direction: alternate-reverse;
+}
+
+</style>
--- src/constant.js
+++ src/constant.js
@@ -15,3 +15,6 @@ export const menuList = [
         icon: "fas fa-users grey-color"
     }
 ];
+
+export const HomeMainText = "TED는 Technology, Entertainment, Design의 약자로 자신이 하는 일에 열정을 가진 사람들이 'Ideas Worth Spreading (공유할 가치가 있는 아이디어)' 이라는 슬로건 아래에서 18분간 발표하고 이야기를 나누는 자리입니다. TEDx의 'x'는 독립적으로 조직된 TED 이벤트를 뜻하며, TED의 기본 가이드라인을 활용하여, 자발적으로 TED를 기획하고자 하는 사람들이 모여 만드는 이벤트입니다.\n\nTED, short for Technology, Entertainment, Design, is a conference where speakers with passion for their work presents their ideas under the slogan 'Ideas Worth Spreading' for 18 minutes. The 'x' in TEDx represents the idea of an independent TED-like event organized by individuals under TED specific guidelines."
+export const TedxRuleVideo = "https://www.youtube.com/embed/QRabMAqxwaE?autoplay=1"
